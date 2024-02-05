1d89f23c769d2b67da85f1af95be61823c78289a
Marcel Busch <marcelbusch@me.com>
2024-02-05 23:36:15 +0100

    initial commit
    working server that takes json encoded data and sends an email with the data

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+const apikey = process.env.APIKEY;
+
+app.post('/contactformsubmit', async (req, res) => {
+  if (!req.body || !req.body.apikey || !req.body.apikey == apikey) {
+    res.send('apikey missing or incorrect');
+    return;
+  }
+
+  const success = await sendmail(req.body).catch(console.error);
+  if (success === true) {
+    res.send("success")
+  } else {
+    res.send("error")
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Contact form forwarding server listening on port ${port}`);
+});
+
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAILPORT,
+  port: process.env.MAILPORT,
+  secure: true,
+  auth: {
+    user: process.env.MAILUSER,
+    pass: process.env.MAILPASS,
+  },
+});
+
+async function sendmail(data) {
+  const info = await transporter.sendMail({
+    from: `"Kontaktformular" <${process.env.MAILUSER}>`, // sender address
+    to: 'me@marcelbusch.de', // list of receivers
+    subject: `Kontaktformular von ${data.name}`, // Subject line
+    text: `name: ${data.name}
+mail: ${data.email}
+message: ${data.message}`, // plain text body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  return true;
+}

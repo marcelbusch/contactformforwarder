c00d67b71d5a66268501fd381574cf71455fc81d
Marcel Busch <marcelbusch@me.com>
2024-04-30 11:46:24 +0200

    ability to send different messages, templates, error handling
    - error handling if request is not valid json
    - ability to send different messages depending on template var
    - templates for message(s) and subject(s)
    - SENDTO email now env var
    - updated docs

--- README.md
+++ README.md
@@ -1,16 +1,58 @@
+# Express Mailer
+
 Node app accepting POST requests and sending the data as email.
 
-Most basic node app for that task that you could imagine.
+Most basic node app for that that you could imagine.
 
 Using [express.js](https://expressjs.com) and [nodemailer](https://nodemailer.com/).
 
 Options set via `.env` file:
-```bash
+
+```sh
 APIKEY=xxx
 PORT=3000
-MAILPASS=xxx
+MAILPASS="xxx"
 MAILUSER=my@mail.address
 MAILPORT=465
 MAILSERVER=smtp.mail.server
+MESSAGE_TEMPLATE="replaces #var# with json body.var"
+SUBJECT_TEMPLATE="replaces #var# with json body.var"
+MESSAGE_TEMPLATE_VAR="if you want multiple templates, send a body.template = \"VAR\" variable"
+SUBJECT_TEMPLATE_VAR="if you want multiple templates, send a body.template = \"VAR\" variable"
+```
 
+If no `SUBJECT_TEMPLATE` is given defaults to `Kontaktformular von #name#` if name is in body, or just `Kontaktformular`.
+If no `MESSAGE_TEMPLATE` is given, defaults to enumerating all body vars in the form of
+
+```yaml
+var1: value1
+var2: value2
+...
+```
+
+## Deploy
+
+### Uberspace
+
+```sh
+uberspace web backend set /contactformsubmit --http --port PORT
+git clone git@github.com:marcelbusch/contactformforwarder.git
+cd contactformforwarder
+npm install
+
+vim ~/etc/services.d/contactformforwarder.ini
+```
+
+```sh
+# ~/etc/services.d/contactformforwarder.ini
+[program:contactformforwarder]
+directory=PATHTO/contactformforwarder
+command=npm run start
+autostart=true
+autorestart=true
+environment=NODE_ENV=production
+```
+
+```sh
+$ supervisorctl restart contactformforwarder
 ```
--- index.js
+++ index.js
@@ -11,18 +11,37 @@ const port = process.env.PORT;
 const apikey = process.env.APIKEY;
 
 app.post('/contactformsubmit', async (req, res) => {
-  console.log(req.body)
-  if (!req.body || !req.body.apikey || !req.body.apikey == apikey) {
+  let body = req.body;
+  // console.log(req);
+  // try {
+  //   body = JSON.parse(req.body);
+  // } catch (e) {
+  //   console.log(e);
+  //   res.send('only send json');
+  //   return;
+  // }
+  console.log(body)
+  console.log(!!body)
+  console.log(Object.keys(body))
+  console.log(Object.keys(body).length)
+  if (!body || !Object.keys(body).length) {
+    console.log(`res.body is empty. request:`);
+    console.log(req)
+    res.send('only send json');
+    return;
+  }
+  console.log(body);
+  if (!body || !body.apikey || !body.apikey == apikey) {
     res.send('apikey missing or incorrect');
     return;
   }
 
-  const success = await sendmail(req.body).catch(console.error);
-  console.log(`success: ${success ? 'true' : 'false'}`)
+  const success = await sendmail(body).catch(console.error);
+  console.log(`success: ${success ? 'true' : 'false'}`);
   if (success === true) {
-    res.send("success")
+    res.send('success');
   } else {
-    res.send("error")
+    res.send('error');
   }
 });
 
@@ -30,6 +49,11 @@ app.listen(port, () => {
   console.log(`Contact form forwarding server listening on port ${port}`);
 });
 
+app.use(function(err, req, res, next) {
+  console.log(err)
+  console.log(req)
+  res.status(500).send('error')
+});
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAILSERVER,
@@ -41,16 +65,38 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+function createMessage(data, template) {
+  if (!template) {
+    let message = '';
+    for (const key in data) {
+      message = message + `${key}: ${data[key]}\n`;
+    }
+    return message;
+  } else {
+    let message = template;
+    for (const key in data) {
+      message = message.replaceAll(`#${key}#`, data[key]);
+    }
+    return message;
+  }
+}
+
 async function sendmail(data) {
-  console.log(data)
-  console.log(transporter)
+  console.log(data);
+  console.log(transporter);
+  delete data.apikey;
+  const msgtmplt = data.template && process.env.hasOwnProperty(`MESSAGE_TEMPLATE_${data.template}`) ? process.env[`MESSAGE_TEMPLATE_${data.template}`] : process.env.MESSAGE_TEMPLATE
+  const sbjttmplt = data.template && process.env.hasOwnProperty(`SUBJECT_TEMPLATE_${data.template}`) ? process.env[`SUBJECT_TEMPLATE_${data.template}`] : process.env.SUBJECT_TEMPLATE
+  if (data.hasOwnProperty('template')) {
+    delete data.template
+  }
+  const message = createMessage(data, msgtmplt);
+  const subject = createMessage(data, sbjttmplt ? sbjttmplt : data.hasOwnProperty('name') ? 'Kontaktformular von #name#' : 'Kontaktformular');
   const info = await transporter.sendMail({
     from: `"Kontaktformular" <${process.env.MAILUSER}>`, // sender address
-    to: 'me@marcelbusch.de', // list of receivers
-    subject: `Kontaktformular von ${data.name}`, // Subject line
-    text: `name: ${data.name}
-mail: ${data.email}
-message: ${data.message}`, // plain text body
+    to: process.env.SENDTO, // list of receivers
+    subject: subject, // Subject line
+    text: message, // plain text body
   });
 
   console.log('Message sent: %s', info.messageId);
